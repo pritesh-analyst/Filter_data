cf4dd92b4be6ee73b13a8809ab27fe3f7f570181
Pritesh shakya <123233989+pritesh-analyst@users.noreply.github.com>
2023-04-18 18:51:23 +0530

    Update Filter_data.py

--- Filter_data.py
+++ Filter_data.py
@@ -1,12 +1,7 @@
 import streamlit as st
+import pandas as pd
+
 def Getdata(substring):
-    import pandas as pd
-    import datetime as dt
-    from pandas import Timestamp
-    from datetime import datetime
-    import math
-    import warnings
-    warnings.filterwarnings('ignore')
     sheet_id="1NikKhqY7u3AGsm9Fpk9UaqNFyzmyojuz8-iqUGh295g"
     sheet_data="Form Responses 1"
 
@@ -20,32 +15,25 @@ def Getdata(substring):
     df['Battery Out Voltage']=pd.to_numeric(df['Battery Out Voltage'])
     df['Amount ']=pd.to_numeric(df['Amount '])
 
-
     data=pd.DataFrame({
                       'Timestamp':df['Timestamp'],'Customer name':df['Customer name'],'Battery_in':df['Battery In'],'Battery_in_volt':df['Battery In Voltage'],'Battery_out':df['Battery Out'],'Battery_Out_volt':df['Battery Out Voltage'],   
                        'Amount':df['Amount '],'Security_amt':df['Security Amount'],'Penalty_amt':df['Penalty Amount '],'Supervisor':df['Shift supervisor'],'Plan':df['Is there any plan?'],
                       'Battery_submit?':df['Is the customer submitting or collecting battery?'],'Center':df['Center']
                        })
 
-#     filtered_data = data[data.apply(lambda row: row.astype(str).str.contains(substring, case=False).any(), axis=1)]
-    filtered_data = data[data.apply(lambda row: any(str(col).lower() == substring.lower() for col in row), axis=1)]
-
-
-
-    
-    return filtered_data
-
-
+    return data
 
 def main():
+    data = Getdata("")
+    unique_entries = []
+    for col in data.select_dtypes(include='object').columns:
+        unique_entries += data[col].unique().tolist()
+    selected_entry = st.selectbox("Select entry", options=unique_entries)
+    filtered_data = data[data.apply(lambda row: any(str(col).lower() == selected_entry.lower() for col in row), axis=1)]
 
-    substring = st.text_input("Filteration Key")
-    after_filteration = Getdata(substring)
-    st.write(after_filteration)
+    # substring = st.text_input("Filteration Key")
+    # after_filteration = filtered_data[filtered_data.apply(lambda row: row.astype(str).str.contains(substring, case=False).any(), axis=1)]
+    st.write(filtered_data)
 
 if __name__ == '__main__':
     main()   
-
-
-
-
